40f5566b860acef23723adbac39a19b796dcad84
Kamogelo Jabulile Motlhale <131041491+JabbzTheCoder@users.noreply.github.com>
2024-10-11 00:04:58 +0200

    push

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,40 +1,26 @@
 #!/usr/bin/python3
+"""
+Solution to lockboxes problem
+"""
+
 
 def canUnlockAll(boxes):
     """
-    Determines if all boxes can be unlocked.
-
-    Args:
-        boxes (list of list of int): A list where each index represents a box,
-                                     and the value at that index is a list of keys
-                                     available inside that box.
-
-    Returns:
-        bool: True if all boxes can be unlocked, otherwise False.
-    
-    The function starts with the first box (index 0) being unlocked.
-    It then uses the keys inside that box to try and unlock other boxes.
-    The process continues by collecting keys from newly unlocked boxes until
-    all boxes are either unlocked or no more keys are available.
+    Determines whether a series of locked boxes can be opened
+    based on keys that can be attained.
+    Solution to the lockboxes problem
     """
-    # Start with all boxes locked, except for the first box
-    unlocked = [False] * len(boxes)
-    unlocked[0] = True  # Box 0 is always unlocked
-    
-    # List of keys we currently possess (initially keys from box 0)
-    keys = boxes[0]
-    
-    # Set to keep track of opened boxes (initially only box 0 is opened)
-    opened = set([0])
-    
-    # Continue unlocking boxes while there are keys to try
-    while keys:
-        key = keys.pop(0)
-        if key < len(boxes) and key not in opened:
-            unlocked[key] = True  # Mark the box as unlocked
-            opened.add(key)  # Add this box to the set of opened boxes
-            # Add keys from the newly unlocked box to the list of keys
-            keys.extend(boxes[key])
-    
-    # Return True if all boxes are unlocked, otherwise False
-    return all(unlocked)
+    if (type(boxes)) is not list:
+        return False
+    elif (len(boxes)) == 0:
+        return False
+
+    for k in range(1, len(boxes) - 1):
+        boxes_checked = False
+        for idx in range(len(boxes)):
+            boxes_checked = k in boxes[idx] and k != idx
+            if boxes_checked:
+                break
+        if boxes_checked is False:
+            return boxes_checked
+    return True
